a595ef53bc62835325e27443f43d0fd860e238b8
m-bartos <m.bartos96@seznam.cz>
2024-01-26 17:28:25 +0100

    Update: philo.c

--- src/philo.c
+++ src/philo.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 13:34:59 by mbartos           #+#    #+#             */
-/*   Updated: 2024/01/26 15:34:54 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/01/26 15:47:36 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ void	grab_right_fork(t_program *program, int philo_index)
 	{
 		program->philos_arr[philo_index].fork_on_table = 0;
 		program->philos_arr[philo_index].hold_right_fork = 1;
-		printf("%d     %d has taken a right fork", program->time, philo_index);
+		pthread_mutex_lock(&program->printf_mutex);
+		printf("%d     %d has taken a right fork\n", program->time, philo_index);
+		pthread_mutex_unlock(&program->printf_mutex);
 	}
 	pthread_mutex_unlock(&program->philos_arr[philo_index].fork_mutex);
 }
@@ -37,7 +39,9 @@ void	grab_left_fork(t_program *program, int philo_index)
 	{
 		program->philos_arr[fork_index].fork_on_table = 0;
 		program->philos_arr[philo_index].hold_left_fork = 1;
-		printf("%d     %d has taken a left fork", program->time, philo_index);
+		pthread_mutex_lock(&program->printf_mutex);
+		printf("%d     %d has taken a left fork\n", program->time, philo_index);
+		pthread_mutex_unlock(&program->printf_mutex);
 	}
 	pthread_mutex_unlock(&program->philos_arr[fork_index].fork_mutex);
 }
@@ -50,12 +54,17 @@ void	*routine(void *program)
 	pthread_mutex_lock(&program2->printf_mutex);
 	printf("Test from thread!\n");
 	pthread_mutex_unlock(&program2->printf_mutex);
+	grab_left_fork(program2, 1);
+	grab_right_fork(program2, 1);
+	grab_left_fork(program2, 0);
+	grab_right_fork(program2, 0);
 	return (NULL);
 }
 
+//check for how many times each philo ate (add nof_eats to t_onephilo)
+
 int	main(int argc, char **argv)
 {
-	t_onephilo	one_philo[5];
 	t_program	program;
 	int			i;
 
@@ -69,13 +78,13 @@ int	main(int argc, char **argv)
 	i = 0;
 	while (i < program.nof_philos)
 	{
-		pthread_create(&one_philo[i].thread, NULL, &routine, &program);
+		pthread_create(&program.philos_arr[i].thread, NULL, &routine, &program);
 		i++;
 	}
 	i = 0;
 	while (i < program.nof_philos)
 	{
-		pthread_join(one_philo[i].thread, NULL);
+		pthread_join(program.philos_arr[i].thread, NULL);
 		i++;
 	}
 	pthread_mutex_destroy(&program.printf_mutex);
