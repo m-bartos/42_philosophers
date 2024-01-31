635cfabaa4924afa8afde9f9c2d388d3d7a5992e
m-bartos <m.bartos96@seznam.cz>
2024-01-31 10:45:01 +0100

    Clean: check_args.c

--- src/check_args.c
+++ src/check_args.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 15:21:12 by mbartos           #+#    #+#             */
-/*   Updated: 2024/01/31 10:15:17 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/01/31 10:44:48 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	args_error(void)
 		"Input must be an integer, non-negative, "
 		"and have the correct number of parameters.\n", 2);
 	printf("Correct syntax: ./philo number_of_philosophers time_to_die "
-			"time_to_eat time_to_sleep "
-			"[number_of_times_each_philosopher_must_eat]\n");
+		"time_to_eat time_to_sleep "
+		"[number_of_times_each_philosopher_must_eat]\n");
 	exit(5);
 }
 
@@ -35,16 +35,16 @@ void	check_args(int argc, char **argv)
 		args_error();
 	if (argc == 5 || argc == 6)
 	{
-	if (ft_int_atoi(argv[1]) < 0)
-		args_error();
-	if (ft_int_atoi(argv[1]) == 0)
-		zero_philo_error();
-	if (ft_int_atoi(argv[2]) < 0)
-		args_error();
-	if (ft_int_atoi(argv[3]) < 0)
-		args_error();
-	if (ft_int_atoi(argv[4]) < 0)
-		args_error();
+		if (ft_int_atoi(argv[1]) < 0)
+			args_error();
+		if (ft_int_atoi(argv[1]) == 0)
+			zero_philo_error();
+		if (ft_int_atoi(argv[2]) < 0)
+			args_error();
+		if (ft_int_atoi(argv[3]) < 0)
+			args_error();
+		if (ft_int_atoi(argv[4]) < 0)
+			args_error();
 	}
 	if (argc == 6)
 	{
