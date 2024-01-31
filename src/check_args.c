62c996295614048c21bed03f2a9d12b203981e5a
m-bartos <m.bartos96@seznam.cz>
2024-01-31 10:14:56 +0100

    Update: check_args.c

--- src/check_args.c
+++ src/check_args.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 15:21:12 by mbartos           #+#    #+#             */
-/*   Updated: 2024/01/25 16:17:36 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/01/31 10:13:58 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 void	args_error(void)
 {
-	ft_putstr_fd("Error: Issue with arguments. Input must be an integer, non-negative, and have the correct number of parameters.", 2);
+	ft_putstr_fd("Error: Issue with arguments. "
+		"Input must be an integer, non-negative, "
+		"and have the correct number of parameters.\n", 2);
+	printf("Correct syntax: ./philo number_of_philosophers time_to_die "
+			"time_to_eat time_to_sleep "
+			"[number_of_times_each_philosopher_must_eat]\n");
 	exit(5);
 }
 
@@ -28,6 +33,8 @@ void	check_args(int argc, char **argv)
 {
 	if (argc < 5)
 		args_error();
+	if (argc == 5 || argc == 6)
+	{
 	if (ft_int_atoi(argv[1]) < 0)
 		args_error();
 	if (ft_int_atoi(argv[1]) == 0)
@@ -38,6 +45,7 @@ void	check_args(int argc, char **argv)
 		args_error();
 	if (ft_int_atoi(argv[4]) < 0)
 		args_error();
+	}
 	if (argc == 6)
 	{
 		if (ft_int_atoi(argv[5]) < 0)
